7d989186d3979c53a93cdec8a405ec98cde20dcb
baezzan <139171359+baezzan@users.noreply.github.com>
2025-06-01 01:42:56 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-// import Login from './pages/Login';
-// import JoinAgree from './pages/JoinAgree';
-// import AdminDashboard from './pages/AdminDashboard';
+import Login from './pages/Login';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/join_agree" element={<JoinAgree />} /> */}
-        {/* <Route path="/admin" element={<AdminDashboard />} /> */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 };
 
